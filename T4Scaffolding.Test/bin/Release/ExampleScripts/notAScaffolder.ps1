5ee4960e0f547c2692877163d2433ef1ede28a00
Cigano Morrison Mendez <ciganogypsy@gmail.com>
2017-05-28 01:23:36 -0500

    Package Updates, Rebuild, Removing bin, obj and ignorable folders.

--- T4Scaffolding.Test/bin/Release/ExampleScripts/notAScaffolder.ps1
+++
@@ -1,4 +0,0 @@
-﻿# Not a scaffolder because it lacks the [T4Scaffolding.Scaffolder()] attribute
-param($myparam, $anotherParam)
-
-return "You entered $myparam then $anotherParam"
--- T4Scaffolding.Test/bin/Release/ExampleScripts/simpleScaffolder.ps1
+++
@@ -1,4 +0,0 @@
-﻿[T4Scaffolding.ControllerScaffolder("My display name", Description = "My description")][CmdletBinding()]
-param($myparam, $anotherParam)
-
-return "You entered $myparam then $anotherParam"
